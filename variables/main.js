d0b96927f33db809c0f1ba5e9f34dc5ab05b1d4f
Ivan Petrov <kepocka@gmail.com>
2019-02-28 19:01:10 +0300

    Create main.js

---
+++ variables/main.js
@@ -0,0 +1,26 @@
+// Задача 1
+let x = 2,
+    y = 22,
+    z = 0,
+    result;
+result = x * y + 5 * z + x - 1;
+console.log(result);
+// Задача 2
+let array = {
+    algebra: 5,
+    geography: 4,
+    physics: 4,
+};
+let average = 0;
+let n = 0;
+for (let key in array) {
+    average += array[key];
+    n++;
+}
+average = average / n;
+
+console.log(average);
+// Задача 3
+
+let name = "Иван";
+console.log("Привет, мир! Меня зовут " + name + ".");
