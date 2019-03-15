1723e40fb6abc48ea382e65cd84b0607c8939b75
Ivan Petrov <kepocka@gmail.com>
2019-03-15 13:22:34 +0300

    Update main.js

--- functions/main.js
+++ functions/main.js
@@ -44,86 +44,71 @@ showSolutionsMessage(3, -18, 27);
 
 
 // Задача 2
-let names = {}
-let secretData = {
-    aaa: 1,
-    bbb: 0
-}
-
-function namingMagic(secretData) {
-    for (key in secretData)
-        if (secretData[key] == 0) {
-            names[key] = "Эмильо"
-        } else if (secretData[key] == 1) {
-        names[key] = "Родриго"
-    }
-
-}
 
 function getPersonData(secretData) {
-    namingMagic(secretData)
-    names.firstName = names.aaa;
-    names.lastName = names.bbb;
-    delete names.aaa;
-    delete names.bbb;
-    console.log(names)
+    let names = {};
+    names.firstName = decipher(secretData.aaa);
+    names.lastName = decipher(secretData.bbb);
+    console.log(names);
 }
-getPersonData(secretData);
+
+function decipher(data) {
+    if (data == 1) {
+        data = "Эмильо"
+    } else if (data == 0) {
+        data = "Родриго"
+    }
+    return data
+}
+
+let code = {
+    aaa: 1,
+    bbb: 0
+};
+getPersonData(code);
 
 // Задача 3
-let data = {
-    algebra: [2, 4, 5, 2, 3, 4],
-    geometry: [2, 4, 5],
-    russian: [3, 3, 4, 5],
-    phycics: [5, 5],
-    music: [2, 2, 6],
-    english: [4, 4, 6],
-    poetry: [5, 3, 4],
-    chemestry: [2],
-    french: [4, 4]
-};
+function disciplineAverage(object) {
+    for (key in object) {
+        object[key] = getAverage(object[key]);
+    }
+    return object;
+}
 
-
-// Задача 3
-let data = {
-    algebra: [2, 4, 5, 2, 3, 4],
-    geometry: [2, 4, 5],
-    russian: [3, 3, 4, 5],
-    phycics: [5, 5],
-    music: [2, 2, 6],
-    english: [4, 4, 6],
-    poetry: [5, 3, 4],
-    chemestry: [2],
-    french: [4, 4]
-};
-
-
-function calculateAverage(array, average) {
+function getAverage(array) {
     let sum = 0;
     for (let i = 0; i < array.length; i++) {
         sum += array[i];
     }
-    sum = sum / array.length
-    average.push(sum)
-    return average;
-};
-
-function getAverageScore(data) {
-    let array = []
-    let average = []
-    for (key in data) {
-        array = data[key];
-        calculateAverage(array, average);
-    };
-    array = average;
-    calculateAverage(array, average);
-    let n = 0;
-    data.average = 0;
-    for (key in data) {
-        data[key] = average[n];
-        n++;
-    }
-    console.log(data);
+    sum = sum / array.length;
+    return sum;
 }
 
-getAverageScore(data);
+function objectDestroyer(object) {
+    let array = []
+    for (key in object) {
+        array.push(object[key]);
+    }
+    return array;
+}
+
+
+function digitalJournal() {
+    let data = {
+        algebra: [2, 4, 5, 2, 3, 4],
+        geometry: [2, 4, 5],
+        russian: [3, 3, 4, 5],
+        phycics: [5, 5],
+        music: [2, 2, 6],
+        english: [4, 4, 6],
+        poetry: [5, 3, 4],
+        chemestry: [2],
+        french: [4, 4]
+    };
+
+    data.average = getAverage(objectDestroyer(disciplineAverage(data)));
+    console.log(data);
+
+
+}
+digitalJournal();
