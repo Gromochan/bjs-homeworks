6898b321a19fff7da17e1e7b4b3eaafe752243f6
Ivan Petrov <kepocka@gmail.com>
2019-03-28 23:13:12 +0300

    Update main.js

--- oop-es6/main.js
+++ oop-es6/main.js
@@ -1,38 +1,55 @@
 class StudentLog {
     constructor(name) {
         this.name = name;
+        this.subject = {};
     }
     getName() {
         return this.name;
     };
     addGrade(grade, subject) {
-        if (grade > 5 || grade < 0 || typeof grade == "string") {
-
+        if (grade > 5 || grade < 0 || typeof grade !== "number") {
             return "Оценка должна быть от 1 до 5!"
         };
-
-        this[subject] = [];
-        this[subject].push(grade)
-
-
+        if (this.subject[subject] !== undefined) {
+            this.subject[subject].push(grade);
+        } else {
+            this.subject[subject] = [];
+            this.subject[subject].push(grade);
+        }
+        console.log(this.subject[subject].length);
+        return
     }
     getAverageBySubject(subject) {
         let sum = 0;
-
-        for (let i = 0; this[subject].length > i; i++) {
-            sum += this[subject][i];
+        for (let i = 0; this.subject[subject].length > i; i++) {
+            sum += this.subject[subject][i];
 
         }
-        sum = sum / this[subject].length;
+        sum = sum / this.subject[subject].length;
+
+        return sum
+    }
+    getTotalAverage(fn) {
+        let i = 0;
+        let sum = 0;
+        console.log(this.subject)
+        for (let key in this.subject) {
+            console.log(this.subject[key])
+            sum += fn(key);
+            i++;
+        }
+        sum = sum / i;
         return sum
     }
 
-    getTotalAverage() {
-        for (this[])
-            if (!)
-    }
 }
 var log = new StudentLog("Вася Пупукин");
-log.addGrade(5, "chemestry")
+log.addGrade(5, "chemestry");
+log.addGrade(5, "chemestry");
+log.addGrade(4, "chemestry");
+log.addGrade(4, "History");
+log.addGrade(3, "History");
+log.addGrade(4, "History");
+log.addGrade(3, "History");
 log.getAverageBySubject("chemestry");
-log.getTotalAverage();
+log.getTotalAverage(log.getAverageBySubject.bind(log));
