95acf29120732269a010333fd57adc21a6827421
Ivan Petrov <kepocka@gmail.com>
2019-04-02 18:22:45 +0300

    Update main.js

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,58 +1,45 @@
 const summ = (a, b) => a + b;
 const mSumm = memoize(summ, 10);
 
-function compareArrays(array1, array2) {
-    if (array1.length == array2.length) {
-        for (let i = 0; i < array1.length; i++) {
-            if (array1[i] != array2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    return false;
-}
-
 function memoize(fn, limit) {
-    let e = fn(3, 5)
     const results = [];
-    return function (a, b, comFn) {
-        for (let i = 0; i < results.length; i++) {
-            console.log(comFn(results[i].args, Array.from(arguments)));
-            if (comFn(results[i].args, Array.from(arguments))) {
-                console.log("Такие данные уже были!")
-                return results[i].result
-            }
-        }
-
-        results.push({
-            args: arguments,
-            result: e
-        });
-        console.log(limit);
-        console.log(results.length);
+    return function (a, b) {
         if (results.length > limit) {
             console.log("Слишком много результатов!")
             results.shift();
         }
-        return results;
+        if (compareArrays(results, arguments)) {
+            console.log("Такие данные уже были!");
+            return console.log(results.find((el, i) => el.args[i]).result)
+        } else {
+            results.push({
+                args: Array.from(arguments),
+                result: fn(a, b)
+            });
+            return console.log(results)
+        }
     }
 }
-mSumm(5, 3, compareArrays)
-mSumm(5, 3, compareArrays)
-mSumm(5, 3, compareArrays)
-mSumm(8, 6, compareArrays)
-mSumm(9, 6, compareArrays)
-mSumm(51, 5, compareArrays)
-mSumm(52, 3, compareArrays)
-mSumm(76, 3, compareArrays)
-mSumm(46, 3, compareArrays)
-mSumm(54, 6, compareArrays)
-mSumm(56, 6, compareArrays)
-mSumm(510, 3, compareArrays)
-mSumm(525, 3, compareArrays)
-mSumm(614, 3, compareArrays)
-mSumm(6, 3, compareArrays)
-mSumm(5, 6, compareArrays)
-mSumm(5, 6, compareArrays)
-mSumm(5, 3, compareArrays)
+
+function compareArrays(array1, array2) {
+    return (array1.some((el, i) => el.args.every((el, i) => el === array2[i])))
+}
+
+mSumm(5, 20)
+mSumm(5, 20)
+mSumm(5, 21)
+mSumm(5, 22)
+mSumm(5, 23)
+mSumm(5, 24)
+mSumm(5, 25)
+mSumm(5, 26)
+mSumm(5, 27)
+mSumm(5, 28)
+mSumm(5, 29)
+mSumm(5, 29)
+mSumm(5, 30)
+mSumm(5, 31)
+mSumm(5, 32)
+mSumm(5, 22)
+mSumm(5, 23)
+mSumm(5, 24)
